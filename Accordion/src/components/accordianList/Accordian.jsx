017c6933260bed88c31448ae92a3b9fccf105d51
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-05-06 14:50:50 +0300

    first cg gen commit

--- Accordion/src/components/accordianList/Accordian.jsx
+++ Accordion/src/components/accordianList/Accordian.jsx
@@ -4,29 +4,35 @@ import data from "./data";
 function Accordian() {
   const [selected, setSelected] = useState(null);
 
-  function handleSelection(getSelectedId) {
-    setSelected(getSelectedId);
+  function handleSingleSelection(selectedId) {
+    setSelected(selected === selectedId ? null : selectedId);
   }
   return (
     <div>
-      {data && data.length ? (
-        data.map((dataItem) => (
-          <div className="item" key={dataItem.id}>
-            <div className="title" onClick={() => handleSelection(dataItem.id)}>
-              <h2>{dataItem.question}</h2>
-              <span>+</span>
-            </div>
+      {data && data.length > 0 ? (
+        <div>
+          {data.map((dataItem) => (
+            <div
+              onClick={() => handleSingleSelection(dataItem.id)}
+              key={dataItem.id}
+            >
+              <div className="titke">
+                <h1>{dataItem.question}</h1>
+                <span>+</span>
+              </div>
 
-            {selected === dataItem.id ? (
-              <div className="answer">{dataItem.answer}</div>
-            ) : null}
-          </div>
-        ))
+              {selected === dataItem.id ? (
+                <div className="answer"> {dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))}
+        </div>
       ) : (
-        <div>No Data Found</div>
-      )}
+        <div>No data Found</div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                       )}
     </div>
   );
 }
 
 export default Accordian;
+  
--- Accordion/src/components/accordianList/data.js
+++ Accordion/src/components/accordianList/data.js
@@ -2,24 +2,28 @@ const data = [
   {
     id: 1,
     question: "what are the accordian components",
-    answer: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?"
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?",
   },
   {
     id: 2,
     question: "what are the accordian components",
-    answer: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?"
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?",
   },
 
   {
     id: 3,
     question: "what are the accordian components",
-    answer: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?"
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?",
   },
 
   {
     id: 4,
     question: "what are the accordian components",
-    answer: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?"
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia assumenda odit laboriosam?",
   },
 ];
 
