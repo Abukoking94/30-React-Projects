bbaef8e88d3e2c03d66bda1689351d053f0fb130
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-05-06 15:47:59 +0300

    Color Generator last commit

---
+++ Color-Generator/Color-Genarator/src/component/ColorGenerator.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+function ColorGenerator() {
+  const [color, setColor] = useState("black");
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+
+  function ColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleRandomHexColor() {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[ColorUtility(hex.length)];
+    }
+    console.log(ColorUtility(16));
+    setColor(hexColor);
+    console.log(color);
+  }
+  function handleRandomRgbColor() {
+    const r = ColorUtility(256);
+    const g = ColorUtility(256);
+    const b = ColorUtility(256);
+
+    setColor(`rgb( ${r} ,${g}, ${b} )`);
+    console.log(color);
+  }
+
+  return (
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: color }}>
+      <button
+        onClick={() => {
+          setTypeOfColor("hex");
+        }}
+      >
+        create Hex Color
+      </button>
+      <button
+        onClick={() => {
+          setTypeOfColor("rgb");
+        }}
+      >
+        create RGB Color
+      </button>
+      <button
+        onClick={() =>
+          typeOfColor === "hex"
+            ? handleRandomHexColor()
+            : handleRandomRgbColor()
+        }
+      >
+        Random Color Generator
+      </button>
+    </div>
+  );
+}
+
+export default ColorGenerator;
+
+/* rgb (255, 255, 255)
+
+0-255
+
+
+
+*/
