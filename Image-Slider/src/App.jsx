27c1fb64db278d45e57d6ec3b970d10ff276183f
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-07-01 16:34:58 +0300

    Last commit for image slider

--- Image-Slider/src/App.jsx
+++ Image-Slider/src/App.jsx
@@ -4,7 +4,11 @@ import ImageSliderApp from "./components/ImageSliderApp";
 function App() {
   return (
     <div>
-      <ImageSliderApp url={"https://picsum.photos/v2/list"} limit={10} />
+      <ImageSliderApp
+        url={"https://picsum.photos/v2/list"}
+        page={1}
+        limit={10}
+      />
     </div>
   );
 }
--- Image-Slider/src/components/ImageSliderApp.jsx
+++ Image-Slider/src/components/ImageSliderApp.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+function ImageSliderApp({ url, limit = 10, page = 1 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-function ImageSliderApp() {
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data && data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") {
+      fetchImages(url);
+    }
+  }, [url]);
+
+  if (loading) {
+    return <h1> The images are loading, please wait...</h1>;
+  }
+
+  if (errorMsg !== null) {
+    <h1> Error Occured {errorMsg}</h1>;
+  }
+
+  function handlePrev() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
   return (
-    <div>ImageSliderApp</div>
-  )
+    <div className="container">
+      <BsArrowLeftCircleFill
+        className="arrow arrow-left"
+        onClick={() => handlePrev()}
+      />
+      {images && images.length
+        ? images.map((imageItem, index) => (
+            <img
+              key={imageItem.id}
+              src={imageItem.download_url}
+              alt={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+
+      <BsArrowRightCircleFill
+        className="arrow arrow-right"
+        onClick={() => handleNext()}
+      />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "circle-indicator"
+                    : "circle-indicator blur-circle-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              >
+                {" "}
+                {index}
+              </button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
 
-export default ImageSliderApp
+export default ImageSliderApp;
