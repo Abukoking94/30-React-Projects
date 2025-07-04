83ad6de52e42b787f6dcc48c8e9160448433d940
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-07-04 10:02:15 +0300

    last commit for LoadMoreBtn

--- Load-More-Function/src/components/LoadMore.jsx
+++ Load-More-Function/src/components/LoadMore.jsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from "react";
 
-function LoadMore({ url, skip, limit }) {
+function LoadMore({ url, limit }) {
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(0);
+  const [disable, setDisable] = useState(false);
 
   async function FetchProducts(getUrl) {
     try {
+      setLoading(true);
       const response = await fetch(
         `${getUrl}?limit=${limit}&skip=${count === 0 ? 0 : count * 20}`
       );
       const data = await response.json();
 
-      data && data.products
-        ? setProduct((prev) => [...prev, ...data.products])
-        : null;
+      if (data && data.products) {
+        setLoading(false);
+        setProduct((prev) => [...prev, ...data.products]);
+      }
     } catch (e) {
       console.log(e.message);
+      setLoading(false);
     }
   }
 
   useEffect(() => {
     FetchProducts(url);
-  }, [url, count]);
+  }, [url, count],1);
+
+  useEffect(() => {
+    product && product.length === 100 ? setDisable(true) : null;
+  }, [product]);
+
   console.log(product);
+
+  if (loading) {
+    return <p>Page is loading...</p>;
+  }
 
   return (
     <div className="container">
@@ -41,13 +54,14 @@ function LoadMore({ url, skip, limit }) {
       </div>
       <div className="load-btn-cont">
         <button
+          disabled={disable}
           onClick={() => setCount((prev) => prev + 1)}
           className="load-btn"
         >
-          {" "}
           Load More
         </button>
       </div>
+      {disable ? <div>You have reached the limit</div> : null}
     </div>
   );
 }
