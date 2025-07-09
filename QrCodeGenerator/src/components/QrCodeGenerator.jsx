0b38769f04f3996ba018e989af7c9745222af5a5
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-07-09 11:42:25 +0300

    Last QR code generator Commit

--- QrCodeGenerator/src/components/QrCodeGenerator.jsx
+++ QrCodeGenerator/src/components/QrCodeGenerator.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
 
 function QrCodeGenerator() {
-  return <div>QrCodeGenerator</div>;
+  const [qrCode, setQrcode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleClick() {
+    setQrcode(input);
+    setInput("");
+  }
+  return (
+    <div>
+      <div>
+        <h1> QR CODE GENERATOR </h1>
+        <input
+          value={input}
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={() => handleClick()}
+        >
+          {" "}
+          Generate
+        </button>
+
+        <QRCode size={400} value={qrCode} />
+      </div>
+    </div>
+  );
 }
 
 export default QrCodeGenerator;
